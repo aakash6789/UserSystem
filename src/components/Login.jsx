971f9ff535073366c3aded9f4a9b64eb995dfa5b
Aakash <aakash2506c@gmail.com>
2024-06-06 22:44:52 +0530

    set up basic authentication using local storage

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
-
+import { useState } from 'react';
 const Login = () => {
+    const [email,setEmail]=useState("");
+    const [password,setPassword]=useState("");
+    const verifyUser=()=>{
+        const fetchedUser=JSON.parse(localStorage.getItem('user'));
+        console.log(fetchedUser.email);
+        if(email == fetchedUser.email && password == fetchedUser.password){
+            console.log("success");
+        }
+    }
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2 text-white">
       {/* <h1>{loading?"Loading":""}</h1> */}
         <label htmlFor="email">email</label>
-                 <input id="email" className="text-black p-2 mb-2 border rounded-lg mb:4 border-gray-300 focus:outline-none focus:border-gray-600 " />
+                 <input id="email" onChange={(e)=>setEmail(e.target.value)} className="text-black p-2 mb-2 border rounded-lg mb:4 border-gray-300 focus:outline-none focus:border-gray-600 " />
         <label htmlFor="password">password</label>
-                 <input id="password" type="password" className="text-black p-2 border rounded-lg mb:4 border-gray-300 focus:outline-none focus:border-gray-600 " />
-                 <button className="p-2 mt-4 border rounded-lg mb:4 border-gray-300 focus:outline-none focus:border-gray-600">Submit</button> 
+                 <input id="password" onChange={(e)=>setPassword(e.target.value)}  type="password" className="text-black p-2 border rounded-lg mb:4 border-gray-300 focus:outline-none focus:border-gray-600 " />
+                 <button className="p-2 mt-4 border rounded-lg mb:4 border-gray-300 focus:outline-none focus:border-gray-600" onClick={verifyUser}>Submit</button> 
     </div>
   )
 }
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
-
+import { useState } from 'react'
+import toast from 'react-hot-toast'
 const Register = () => {
+ const [username,setUsername]=useState("");
+ const [email,setEmail]=useState("");
+ const [password,setPassword]=useState("");
+    const saveUser=()=>{
+           localStorage.setItem('user',JSON.stringify({email:email,password:password}));
+           toast.success('User created successfully');
+           
+    }
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2 text-white">
               {/* <h1>{loading?"Processing":"Sign up"}</h1> */}
                  <hr></hr>
-                 <label htmlFor="username">username</label>
-                 <input id="username" className="text-black p-2 border rounded-lg mb:4 border-gray-300 focus:outline-none focus:border-gray-600 " />
+                 <label htmlFor="username" >username</label>
+                 <input id="username" onChange={(e)=>setUsername(e.target.value)} className="text-black p-2 border rounded-lg mb:4 border-gray-300 focus:outline-none focus:border-gray-600 " />
                  <label htmlFor="email">email</label>
-                 <input id="email" className="text-black p-2 border rounded-lg mb:4 border-gray-300 focus:outline-none focus:border-gray-600 "/>
+                 <input id="email"onChange={(e)=>setEmail(e.target.value)} className="text-black p-2 border rounded-lg mb:4 border-gray-300 focus:outline-none focus:border-gray-600 "/>
         <label htmlFor="password">password</label>
-                 <input id="password" type="password" className="text-black p-2 border rounded-lg mb:4 border-gray-300 focus:outline-none focus:border-gray-600 "/>
-                 <button className="p-2 mt-4 border rounded-lg mb:4 border-gray-300 focus:outline-none focus:border-gray-600" >Submit</button>
+                 <input id="password" onChange={(e)=>setPassword(e.target.value)} type="password" className="text-black p-2 border rounded-lg mb:4 border-gray-300 focus:outline-none focus:border-gray-600 "/>
+                 <button className="p-2 mt-4 border rounded-lg mb:4 border-gray-300 focus:outline-none focus:border-gray-600" onClick={saveUser} >Submit</button>
     </div>
   )
 }
